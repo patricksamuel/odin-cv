f666154b89ded7061e02e1dc442077927b36e30c
Patrick Samuel <10101460+patricksamuel@users.noreply.github.com>
2025-09-07 22:29:05 +0200

    add school

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,16 @@ import { useState } from 'react'
 import './App.css'
 import GeneralInfoForm from './components/generalInfoForm'
 import GeneralInfoRender from './components/generalInfoRender';
+import SchoolInfoForm from './components/schoolInfoForm';
 
 function App() {
+  const [schoolInfoList, setschoolInfoList] = useState([])
+
+  function addSchool(newSchool){
+    setschoolInfoList((prev)=>[...prev,newSchool])
+  }
+
+
   const [generalInfo, setGeneralInfo] = useState({
     firstName : '',
     lastName : '',
@@ -13,6 +21,8 @@ function App() {
     Email : '',
     phoneNumber : '',
   });
+
+
 
   function handleChange(e) {
       const { name, value} = e.target;
@@ -22,18 +32,34 @@ function App() {
       }))
   }
 
+  function schoolHandleChange(e) {
+    return
+  }
+
   return (
 
      <div className='app-container'>
       <div className='form-section'>
-
+        <h2>Personal Information</h2>
         <GeneralInfoForm
           generalInfo = {generalInfo}
           handleChange = {handleChange}
         />
+        <h2>School</h2>
+        <SchoolInfoForm
+          addSchool = {addSchool}
+        />
       </div>
       <div className='preview-section'>
         <GeneralInfoRender generalInfo={generalInfo}/>
+        <h2>Education</h2>
+        <ul>
+          {schoolInfoList.map((school,index) => (
+            <li key ={index}>
+              {school.degree}
+            </li>
+          ))}
+        </ul>
       </div>
     </div> 
   );
---
+++ src/components/schoolInfoForm.jsx
@@ -0,0 +1,97 @@
+// schoolInfoForm.jsx
+
+import { useState } from "react"
+
+
+export default function SchoolInfoForm({ addSchool }){
+    const [schoolInfo, setSchoolInfo] = useState({
+        degree : '',
+
+    });
+    function handleChange(e) {
+        const { name, value } = e.target;
+        setSchoolInfo((prev) => ({
+            ...prev,
+            [name]: value,
+        }));
+    }
+    
+    function handleClick() {
+        addSchool(schoolInfo)
+        setSchoolInfo({degree :""})
+    }
+
+    
+    return(
+        <>
+            <form className="row g-3">
+                <div className="col-md-6">
+                    <label htmlFor="validationServer01" className="form-label">Degree Program</label>
+                    <input
+                        type="text"
+                        className="form-control is-valid"
+                        id="validationServer01"
+                        name="degree"
+                        placeholder="M.Sc"
+                        value={schoolInfo.degree}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                {/*
+                <div className="col-md-6">
+                    <label htmlFor="validationServer02" className="form-label">Major</label>
+                    <input
+                        type="text"
+                        className="form-control is-valid"
+                        id="validationServer02"
+                        placeholder="Chemical Engineering"
+                        name="Major"
+                        value={schoolInfo.lastName}
+                        onChange={schoolHandleChange}
+                        required
+                    />
+                </div>
+                */}
+            </form> 
+
+            {/*
+            <form className="row g-3">
+                <div className="col-md-12">
+                    <label htmlFor="validationServer01" className="form-label">University</label>
+                    <input
+                        type="text"
+                        className="form-control is-valid"
+                        id="validationServer01"
+                        name="university"
+                        placeholder="Harvard University"
+                        value={schoolInfo.university}
+                        onChange={schoolHandleChange}
+                        required
+                    />
+                </div>
+            </form>
+            <form className="row g-3">
+                <div className="col-md-6">
+                    <label htmlFor="validationServer01" className="form-label">Year</label>
+                    <input
+                        type="number"
+                        className="form-control is-valid"
+                        id="graduateYear"
+                        name="graduateYear"
+                        placeholder="2025"
+                        value={schoolInfo.graduateYear}
+                        onChange={schoolHandleChange}
+                        required
+                    />
+                </div>
+        
+            </form>
+            */}
+
+            <button onClick={handleClick} className="btn btn-primary">Add New</button>
+        </>
+
+        
+    )
+}
